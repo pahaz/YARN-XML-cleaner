92562a31f60717c302b5c3e7d9f74561a3e8cda5
Dmitry Ustalov <dmitry@eveel.ru>
2014-05-29 22:14:30 +0600

    Add the putback.rb stub

---
+++ putback.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# Please note that this script should be executed within
+# the YARN application context in such a manner as
+#   dmchk@tazik:~/Work/yarn$ rails r extract.rb
+
+require 'csv'
+
+CSV.open('current_words.cleaned.csv') do |csv|
+
+end
+
+CSV.open('current_definitions.cleaned.csv') do |csv|
+
+end
+
+CSV.open('current_examples.cleaned.csv') do |csv|
+
+end
