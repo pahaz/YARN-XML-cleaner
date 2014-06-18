8b13d3e1c526d6b1bbadda1efa9c61f9658a745c
Dmitry Ustalov <dmitry@eveel.ru>
2014-06-19 02:16:56 +0600

    Complete putback.rb

--- putback.rb
+++ putback.rb
@@ -3,18 +3,31 @@
 
 # Please note that this script should be executed within
 # the YARN application context in such a manner as
-#   dmchk@tazik:~/Work/yarn$ rails r extract.rb
+#   dmchk@tazik:~/Work/yarn$ rails r putpack.rb
 
 require 'csv'
+require 'pry'
 
-CSV.open('current_words.cleaned.csv') do |csv|
+Dir.chdir(File.expand_path('../', __FILE__)) do
+  Word.transaction do
+    break
+    CSV.foreach('current_words.cleaned.csv', headers: true) do |row|
+      next if row['word'].blank?
+      Word.where(id: row['id']).update_all(word: row['word'])
+    end
+  end
 
-end
+  Definition.transaction do
+    CSV.foreach('current_definitions.cleaned.csv', headers: true) do |row|
+      next if row['text'].blank?
+      Definition.where(id: row['id']).update_all(source: row['source'], text: row['text'])
+    end
+  end
 
-CSV.open('current_definitions.cleaned.csv') do |csv|
-
-end
-
-CSV.open('current_examples.cleaned.csv') do |csv|
-
+  Example.transaction do
+    CSV.foreach('current_examples.cleaned.csv', headers: true) do |row|
+      next if row['text'].blank?
+      Example.where(id: row['id']).update_all(source: row['source'], text: row['text'])
+    end
+  end
 end
